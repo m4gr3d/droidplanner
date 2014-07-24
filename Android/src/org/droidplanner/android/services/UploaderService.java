fc599869a42dc80594438ba6018ffe972391a5af
fhuya <fhuyakou@gmail.com>
2014-07-24 10:57:43 -0400

    Merge branch 'feature_android_wear' of https://github.com/ne0fhyk/droidplanner into feature_glass
    Conflicts:
    	Android/src/org/droidplanner/android/fragments/SettingsFragment.java
    	Android/src/org/droidplanner/android/services/UploaderService.java
    	Android/src/org/droidplanner/android/utils/prefs/DroidPlannerPrefs.java

--- Android/src/org/droidplanner/android/services/UploaderService.java
+++ Android/src/org/droidplanner/android/services/UploaderService.java
@@ -5,7 +5,7 @@ import java.io.File;
 import org.apache.http.client.HttpResponseException;
 import org.droidplanner.R;
 import org.droidplanner.android.communication.service.NetworkConnectivityReceiver;
-import org.droidplanner.android.utils.file.DirectoryPath;
+import org.droidplanner.android.lib.utils.file.DirectoryPath;
 import org.droidplanner.android.utils.prefs.DroidPlannerPrefs;
 
 import android.app.IntentService;
